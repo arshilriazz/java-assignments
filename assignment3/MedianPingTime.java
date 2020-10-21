f5ca45375e50090521b8ea72b0f6f04f86c8703f
Arshil Riaz <arshil.riaz@zemosolabs.com>
2020-10-21 16:54:41 +0530

    made changes

--- assignment3/MedianPingTime.java
+++ assignment3/MedianPingTime.java
@@ -18,6 +18,10 @@ public class MedianPingTime {
                     times.add(time);
                 }
             }
+            if(times.size() == 0) {
+                System.out.println("Name or service not known");
+                return times;
+            }
             times.remove(times.size()-1);
             process.waitFor();
             return times;
@@ -29,22 +33,26 @@ public class MedianPingTime {
     }
 
     public static double median(List<Double> times) {
-        Collections.sort(times);
-        if(times.size() % 2 == 1) {
-            return times.get(times.size()/2 + 1);
+        if(times.size() != 0) {
+            Collections.sort(times);
+            if (times.size() % 2 == 1) {
+                return times.get(times.size() / 2 + 1);
+            } else {
+                return (times.get(times.size() / 2 + 1) + times.get(times.size() / 2)) / 2;
+            }
         }
-        else {
-            return (times.get(times.size()/2 + 1) + times.get(times.size()/2 )) / 2;
-        }
+        else return 0;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the website");
+        System.out.println("Enter the host");
         String host = scanner.nextLine();
         String command = "ping -c 10 " + host;
         List<Double> res = getPingTimes(command);
-        System.out.println(res);
-        System.out.println(median(res));
+        if(res.size() != 0) {
+            System.out.println(res);
+            System.out.println(median(res));
+        }
     }
 }
--- assignment5/Main.java
+++ assignment5/Main.java
@@ -1,13 +1,9 @@
 package assignments.assignment5;
 
-import assignments.assignment5.singleton.Second;
-
 public class Main {
     public static void main(String[] args) {
         First first = new First();
         first.printClassVariables();
         first.printLocalVariables();
-        Second second = Second.initialize("hello");
-        second.printString();
     }
 }
